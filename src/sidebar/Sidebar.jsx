0820c72132b57d5fd7d5e65c79ca9ab5109ab30d
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-02-20 07:56:01 +0330

    add sidebar

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://i.ibb.co/tPhKKhF/pexels-zoe-pappas-1006965.jpg"
+          alt=""
+        />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+          laudantium enim eligendi cupiditate libero distinctio alias iusto
+          asperiores?
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Tech</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+          <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+          <i className="sidebarIcon fa-brands fa-whatsapp-square"></i>
+          <i className="sidebarIcon fa-brands fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
