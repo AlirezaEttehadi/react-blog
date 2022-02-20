11a747291a7d47e9f78636bf710556a3d557218e
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-02-20 07:08:38 +0330

    add header and home page

---
+++ src/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" src="https://i.ibb.co/n8dWBdJ/pexels-donald-tong-33443.jpg" alt="" />
+    </div>
+  )
+}
