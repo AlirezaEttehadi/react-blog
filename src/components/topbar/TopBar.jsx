9a81661c7f9340f85537dc8093cb0ae3db83124b
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-02-23 08:07:20 +0330

    add react router dom

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
 import "./topbar.css";
+import { Link } from "react-router-dom";
 
 export default function TopBar() {
+  const user = false;
   return (
     <div className="top">
       <div className="topLeft">
@@ -11,20 +13,51 @@ export default function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
         </ul>
       </div>
       <div className="topRight">
-        <img
-          src="https://i.ibb.co/zxLVmqs/pexels-nikita-krasnov-6065314.jpg"
-          alt="pexels-nikita-krasnov-6065314"
-          border="0"
-          className="topImage"
-        />
+        {user ? (
+          <img
+            src="https://i.ibb.co/zxLVmqs/pexels-nikita-krasnov-6065314.jpg"
+            alt="pexels-nikita-krasnov-6065314"
+            border="0"
+            className="topImage"
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,35 @@
 import "./register.css";
-
+import { Link } from "react-router-dom";
 export default function Register() {
   return (
     <div className="register">
       <span className="registerTitle">register</span>
       <form className="registerForm">
         <label>Username</label>
-        <input className="registerInput" type="text" placeholder="Enter your username..." />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your username..."
+        />
         <label>Email</label>
-        <input className="registerInput" type="text" placeholder="Enter your email..." />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your email..."
+        />
         <label>Password</label>
-        <input className="registerInput" type="password" placeholder="Enter your password..." />
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password..."
+        />
         <button className="registerButton">register</button>
       </form>
-      <button className="registerLoginButton">Login</button>
+      <button className="registerLoginButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
     </div>
   );
 }
