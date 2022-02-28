6a69af0d6d0a6df2a059cbc4cf5ff74215705a3f
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-02-28 08:11:32 +0330

    add login api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcrypt");
 //REGISER
 router.post("/register", async (req, res) => {
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -19,4 +19,21 @@ router.post("/register", async (req, res) => {
   }
 });
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(400).json("Wrong credentials!");
+    }
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) {
+      res.status(400).json("Wrong credentials!");
+    }
+    const {password , ...others } = user._doc
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
