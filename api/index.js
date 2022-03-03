ed82f4f2890e41a875bb823afa88489cdf534980
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-03-03 20:53:22 +0330

    add categories crud

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users")
-const postRoute = require("./routes/posts")
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json())
@@ -17,6 +18,7 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("4000", () => {
   console.log("backend is running...");
