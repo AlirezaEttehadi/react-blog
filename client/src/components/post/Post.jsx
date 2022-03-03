3e6b6bb7e835e9f8c77a35de15970306a66742d1
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-03-04 00:19:42 +0330

    add axios and post completed

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,36 +1,22 @@
 import "./post.css";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://i.ibb.co/Ny59Mcb/pexels-je-shoots-126292.jpg"
-        alt=""
-      />
+      {post.photo ? <img className="postImg" src={post.photo} alt="" /> : null}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((category) => (
+            <span className="postCat">{category.name}</span>
+          ))}
         </div>
-        <span className="postTitle">Lorem, ipsum dolor sit amet</span>
+        <span className="postTitle">{post.title}</span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
-        corrupti libero? Nulla numquam quas iure libero in at consectetur rerum
-        deserunt blanditiis pariatur, tempore voluptatum molestias consequatur,
-        magnam cupiditate.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
-        corrupti libero? Nulla numquam quas iure libero in at consectetur rerum
-        deserunt blanditiis pariatur, tempore voluptatum molestias consequatur,
-        magnam cupiditate.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo
-        corrupti libero? Nulla numquam quas iure libero in at consectetur rerum
-        deserunt blanditiis pariatur, tempore voluptatum molestias consequatur,
-        magnam cupiditate.
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
