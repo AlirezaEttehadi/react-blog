983e9fa9ac9a3e756e6d7a35ad7573cd2f49737e
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-03-03 20:18:33 +0330

    get all posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -43,8 +43,8 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
-        await post.delete()
-        res.status(200).json("Your post has been deleted!")
+        await post.delete();
+        res.status(200).json("Your post has been deleted!");
       } catch (error) {
         res.status(500).json(error);
       }
@@ -58,12 +58,33 @@ router.delete("/:id", async (req, res) => {
 
 //GET
 router.get("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      res.status(200).json(post)
-    } catch (error) {
-      res.status(500).json(error);
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({
+        username,
+      });
+    } else if (catName) {
+      posts = await Post.find({ categories: { $in: [catName] } });
+    } else {
+      posts = await Post.find();
     }
-  });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
