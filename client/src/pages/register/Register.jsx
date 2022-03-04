90704a4cf87172206f622f439a4dd232db8f60b8
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-03-04 16:30:35 +0330

    add register logic api

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,35 +1,68 @@
 import "./register.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const handleSubmit = async (event) => {
+    setError(false);
+    event.preventDefault();
+    try {
+      const res = await axios.post("/auth/register/", {
+        username,
+        email,
+        password,
+      });
+      if (res.data) {
+        window.location.replace("/login");
+      }
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className="register">
       <span className="registerTitle">register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your username..."
+          onChange={(event) => setUsername(event.target.value)}
         />
         <label>Email</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your email..."
+          onChange={(event) => setEmail(event.target.value)}
         />
         <label>Password</label>
         <input
           className="registerInput"
           type="password"
           placeholder="Enter your password..."
+          onChange={(event) => setPassword(event.target.value)}
         />
-        <button className="registerButton">register</button>
+        <button className="registerButton" type="submit">
+          register
+        </button>
       </form>
       <button className="registerLoginButton">
         <Link className="link" to="/login">
           Login
         </Link>
       </button>
+      {error ? (
+        <span style={{ color: "red", marginTop: 10 }}>
+          Something went wrong!
+        </span>
+      ) : null}
     </div>
   );
 }
